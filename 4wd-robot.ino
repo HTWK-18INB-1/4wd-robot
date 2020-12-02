d26158e2a6afc5b0c53a0337581d3263dfb5b397
Vivien Richter <vivien-richter@outlook.de>
2020-12-02 18:13:08 +0100

    added: empty Arduino IDE sketch

---
+++ 4wd-robot.ino
@@ -0,0 +1,12 @@
+/*
+  4WD robot with sonar and optical position detection.
+
+  2020 by Vivien Richter <vivien-richter@outlook.de>
+  https://github.com/vivi90/4wd-robot
+*/
+
+// Initialization
+void setup() {}
+
+// Main
+void loop() {}
